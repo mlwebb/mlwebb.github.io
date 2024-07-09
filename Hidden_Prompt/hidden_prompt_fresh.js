ab62d65eebecb95958ef44c07c704fb86fd371de
mlwebb1 <mlwebb1@uci.edu>
2024-07-09 14:47:29 -0700

    asdfasdf

--- Hidden_Prompt/hidden_prompt_fresh.js
+++ Hidden_Prompt/hidden_prompt_fresh.js
@@ -6,7 +6,7 @@ define(function () {
       (oControlHost.configuration && oControlHost.configuration["Parameter"]) ||
       "pl";
     console.log(this.m_sParameterName);
-    setTimeout(this.f_submit.bind(this, oControlHost), 0);
+    setTimeout(this.f_submit.bind(this, oControlHost), 10);
   };
   C_HiddenPrompt.prototype.f_submit = function (oControlHost) {
     oControlHost.valueChanged();
@@ -17,23 +17,12 @@ define(function () {
     console.log(this.m_oDataStore);
   };
   C_HiddenPrompt.prototype.getParameters = function () {
-    console.log("yo whats uPPPPPPPPPPPP");
-    console.log(oDataStore);
-    console.log(this.m_oDataStore);
-    console.log(this.m_sParameterName);
     return [
       {
         parameter: this.m_sParameterName,
         values: [{ use: this.m_oDataStore.getCellValue(0, 0) }],
       },
     ];
-  };
-
-  C_HiddenPrompt.prototype.getDatastore = function (oControlHost, oDataStore) {
-    if (this.m_oDataStore || oDataStore) {
-      this.m_oDataStore = oDataStore;
-      console.log("DatastoreName", this.m_oDataStore.name || "none");
-    }
   };
   return C_HiddenPrompt;
 });
